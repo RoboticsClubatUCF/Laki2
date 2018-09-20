e1eae96c4798b23370704c379148ae08f92197fb
Wesley Fletcher <wesfletch@gmail.com>
2018-09-20 17:29:56 -0400

    Adding setup script for new members

---
+++ fresh_setup.sh
@@ -0,0 +1,30 @@
+
+#!/bin/sh
+
+sudo apt-get -y install software-properties-common
+#echo "export ROS_OS_OVERRIDE=elementary" >> ~/.bashrc
+#. ~/.bashrc
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo add-apt-repository -y ppa:webupd8team/atom
+sudo apt-get -y update
+sudo apt-get -y install ros-kinetic-desktop-full
+sudo apt-get -y install atom
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+. ~/.bashrc
+sudo apt-get -y install python-rosinstall python-rosinstall-generator python-wstool build-essential ros-lunar-catkin
+cd
+git clone https://github.com/RoboticsClubatUCF/Laki2.git
+
+cd
+
+sudo apt-get -y install python-jinja2
+sudo apt-get -y install python-pip python-dev
+sudo pip install numpy toml
+sudo pip install opencv-python
+sudo apt-get -y install python-opencv
+sudo apt-get -y install git
+
+. ~/.bashrc
